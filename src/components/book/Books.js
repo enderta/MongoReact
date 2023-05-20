4effecbf98e8807fc166a36cdec722175ba29d29
enderta <endertanver@gmail.com>
2023-05-20 13:35:10 +0100

    Update files

--- src/components/book/Books.js
+++ src/components/book/Books.js
@@ -1,57 +1,76 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
-import {Button} from "react-bootstrap";
+import { Button } from 'react-bootstrap';
+
 function Books(props) {
-const [books, setBooks] = useState([]);
-const [check, setCheck] = useState(false);
+    const [books, setBooks] = useState([]);
 
-const handleCheck = () => setCheck(!check);
-
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
     const client = new ApolloClient({
         uri: 'http://localhost:5000/graphql',
         cache: new InMemoryCache()
     });
 
-    client
-        .query({
-            query: gql`
-        query {
+    const fetchBooks = () => {
+        client
+            .query({
+                query: gql`
+          query {
             books {
+              name
+              genre
+              author {
                 name
-                genre
-                author {
-                    name
-                    }
-                    
-        }
-    }
-    `
-        })
-        .then(result => setBooks(result.data.books));
+              }
+            }
+          }
+        `
+            })
+            .then(result => {
+                const booksWithCheck = result.data.books.map(book => ({
+                    ...book,
+                    check: false
+                }));
+                setBooks(booksWithCheck);
+            });
+    };
 
+    const handleCheck = index => {
+        setBooks(prevBooks => {
+            const updatedBooks = [...prevBooks];
+            updatedBooks[index] = {
+                ...updatedBooks[index],
+                check: !updatedBooks[index].check
+            };
+            return updatedBooks;
+        });
+    };
 
     return (
         <div>
             <h1>Reading List</h1>
             <div className={'container'}>
                 <div className={'row'}>
-                    {books.map(book => (
-                        <div className={'col-md-4'}>
-                            <br/>
+                    {books.map((book, index) => (
+                        <div className={'col-md-4'} key={index}>
+                            <br />
                             <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">{book.name}</h5>
                                     <h6 className="card-subtitle mb-2 text-muted">{book.genre}</h6>
                                     <p className="card-text">{book.author.name}</p>
-                                    {
-                                        check ?
-                                            //if true show this as read check
-                                        <h1 onClick={handleCheck}>Not Read X </h1>
-                                            :
-                                            //if false show this as not read check
-                                        <h1 onClick={handleCheck}>Read ✅ </h1>
-                                    }
+                                    {book.check ? (
+                                        <h6 className="card-subtitle mb-2 text-muted" onClick={() => handleCheck(index)}>
+                                            Read ✅
+                                        </h6>
+                                    ) : (
+                                        <h6 className="card-subtitle mb-2 text-muted" onClick={() => handleCheck(index)}>
+                                            Not Read ❌
+                                        </h6>
+                                    )}
                                 </div>
                             </div>
                         </div>
